aa03a2b4756140ca91390b3acd72ed7ee4786cbe
xaviermaldonadony <xaviermaldonadony@hotmail.com>
2020-10-22 23:58:49 -0400

    Adding project

---
+++ src/with-data.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const withData = (WrappedComponent, dataSource) => {
+	class WithData extends React.Component {
+		constructor() {
+			super();
+			this.state = {
+				data: [],
+			};
+		}
+
+		componentDidMount() {
+			setTimeout(() => {
+				fetch(this.props.dataSource)
+					.then((response) => response.json())
+					.then((data) => this.setState({ data: data.slice(0, 3) }));
+			}, 1500);
+		}
+		render() {
+			const { dataSource, ...otherProps } = this.props;
+
+			return this.state.data.length < 1 ? (
+				<h1>LOADING</h1>
+			) : (
+				<WrappedComponent data={this.state.data} {...otherProps} />
+			);
+		}
+	}
+	return WithData;
+};
+
+export default withData;
